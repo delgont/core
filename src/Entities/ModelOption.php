18d33daffa9fa813427622716064e70ffcbdc3c4
Stephen Okello Omoding <okellostephenomoding@gmail.com>
2025-02-16 20:23:07 +0300

    v1.8.1

--- src/Entities/ModelOption.php
+++ src/Entities/ModelOption.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Delgont\Core\Entities\Option;
+namespace Delgont\Core\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 
--- src/helpers.php
+++ src/helpers.php
@@ -1,9 +1,19 @@
 <?php
 
 use Delgont\Core\Repository\OptionRepository;
+use Delgont\Core\Entities\Any;
+
 
 if(!function_exists('option')){
     function option($option_key, $group, $default = null){
         return app(OptionRepository::class)->fromCache()->findOption($option_key, $group)->value ??  $default;
     }
 }
+
+
+if(!function_exists('any')){
+    function any($any){
+        return new Any($any);
+    }
+}
+
